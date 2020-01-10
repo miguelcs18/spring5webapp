aa8b312b793ae6380f0a08fa5258291999cad154
miguelcs18 <35112583+miguelcs18@users.noreply.github.com>
2020-01-10 13:56:09 -0600

    This is my first commit

---
+++ src/main/java/guru/springframework/spring5webapp/Model/Book.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring5webapp.Model;
+
+import java.util.Objects;
+
+public class Book {
+
+    private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
